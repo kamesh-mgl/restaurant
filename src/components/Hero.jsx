6bf86dfe631745002c20b5d17a17c06bed975553
kamesh-mgl <kameshmgl@gmail.com>
2025-11-19 19:14:54 +0530

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,17 @@
 import { Link } from "react-router-dom";
 
-export default function Hero()
-{
+export default function Hero() {
 
-    return(
+    return (
 
         <div id="home" className="container">
             <div className="banner">
-                <h1>Welcome to Spice Garden</h1>
+                <h1>Welcome to <strong>Spice Garden</strong> </h1>
                 <span>Where taste means tradition</span>
                 <p id="hero-p">Step into The Spice Garden, where every dish is crafted with love, passion,
-                     and authentic flavors. From aromatic Indian curries to sizzling tandoori specials,
-                      we bring you the perfect blend of tradition and taste. 
-                      Our chefs use only the freshest ingredients to 
+                    and authentic flavors. From aromatic Indian curries to sizzling tandoori specials,
+                    we bring you the perfect blend of tradition and taste.
+                    Our chefs use only the freshest ingredients to
                     create dishes that delight your senses and make every meal memorable.</p>
             </div>
             {/* <a href="#menu" className="anchor">Explore Menu</a> */}
